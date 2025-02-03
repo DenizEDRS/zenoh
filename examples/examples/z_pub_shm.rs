79d703ba8604d260dbc644b01ef01fac83a5c5a3
Deniz Erman Dursun <58140264+DenizEDRS@users.noreply.github.com>
2025-02-03 20:33:34 +0100

    Update z_pub_shm.rs

--- examples/examples/z_pub_shm.rs
+++ examples/examples/z_pub_shm.rs
@@ -1,26 +1,3 @@
-// Copyright (c) 2023 ZettaScale Technology
-
-//
-
-// This program and the accompanying materials are made available under the
-
-// terms of the Eclipse Public License 2.0 which is available at
-
-// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
-
-// which is available at https://www.apache.org/licenses/LICENSE-2.0.
-
-//
-
-// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
-
-//
-
-// Contributors:
-
-//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
-
-//
 
 use clap::Parser;
 
@@ -118,7 +95,7 @@ async fn main() -> zenoh::Result<()> {
 
     for idx in 0..u32::MAX {
 
-        tokio::time::sleep(std::time::Duration::from_micros(1)).await;
+        
 
 
 
@@ -225,3 +202,4 @@ fn parse_args() -> (Config, KeyExpr<'static>, String) {
     (args.common.into(), args.key, args.payload)
 
 }
+
